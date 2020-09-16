99f8c34d57d9db4c37595efec33a48aaa0c0b8bb
ecenin09 <ecenin09@gmail.com>
2020-09-16 10:14:02 +0300

    Rotation + UI

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Range(1,100)] public float speed;
+    public FixedJoystick joystick;
+    private Vector3 _direction;
+    private void Start()
+    {
+        if (joystick == null)
+        {
+            joystick = FindObjectOfType<FixedJoystick>();
+        }
+
+    }
+
+    private void Update()
+    {
+        _direction = joystick.Direction*(Time.deltaTime*speed);
+        transform.RotateAround(transform.position,transform.up,_direction.x);
+        transform.RotateAround(transform.position,transform.right,-_direction.y);
+    }
+    
+
+}
