79b638cceba6dfcba7782e55db9e6a5135e2915b
ecenin09 <ecenin09@gmail.com>
2020-09-16 13:54:33 +0300

    Add Rotation and Shoot

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float fireForce = 25.0f;
+    void Start()
+    {
+        gameObject.GetComponent<Rigidbody>().AddForce(transform.forward*fireForce,ForceMode.Impulse);        
+    }
+
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Player : MonoBehaviour
 {
-    [Range(1,100)] public float speed;
+    
+    private float _tiltAngle = 60.0f;
     public FixedJoystick joystick;
     private Vector3 _direction;
+    public GameObject[] prefabArray;
+    public Transform firePlace;
     private void Start()
     {
         if (joystick == null)
@@ -19,10 +23,17 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        _direction = joystick.Direction*(Time.deltaTime*speed);
-        transform.RotateAround(transform.position,transform.up,_direction.x);
-        transform.RotateAround(transform.position,transform.right,-_direction.y);
+        _direction =joystick.Direction;
+
+        transform.RotateAround(transform.position, Vector3.up*_direction.x, _tiltAngle * Time.deltaTime);
+        transform.Rotate(Vector3.left*_direction.y,_tiltAngle*Time.deltaTime);
     }
-    
+
+    public void Fire()
+    {
+        GameObject bullet = prefabArray[Random.Range(0, prefabArray.Length)];
+        Instantiate(bullet, firePlace.position, transform.rotation);
+        
+    }
 
 }
